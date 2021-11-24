15ba81f3ef5bf6b8f56f8e6ecee5cda4cb86bf06
Mayank <mayankgupta8900@gmail.com>
2021-11-24 11:12:02 +0530

    2d and leetcode

--- 2d-array/RotateNintyDeegre.java
+++ 2d-array/RotateNintyDeegre.java
@@ -3,42 +3,42 @@ import java.util.*;
 public class RotateNintyDeegre {
 
     public static void main(String[] args) throws Exception {
-        Scanner scn = new Scanner (System.in);
+        Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int [][]arr=new int [n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j]=scn.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scn.nextInt();
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-               int temp= arr[i][j];
-                arr[i][j]=arr[j][i];
-                arr[j][i]=temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
-        for(int i=0;i<arr.length;i++){
-            int left =0;
-            int right = arr[i].length-1;
-            while(left<right){
+        for (int i = 0; i < arr.length; i++) { 
+            int left = 0;
+            int right = arr[i].length - 1;
+            while (left < right) {
                 int temp = arr[i][left];
-                arr[i][left]=arr[i][right];
-                arr[i][right]=temp;
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
                 left++;
                 right--;
             }
         }
         display(arr);
+        scn.close();
     }
 
-    public static void display(int[][] arr){
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[0].length; j++){
+    public static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
-
 }
